ebae8faacc28b86228cd2d42f8029fcdf849a63d
Jason-Iverson.com <contact@jason-iverson.com>
2017-11-04 13:39:08 -0700

    Updated assembly information

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JM0ney.Framework.Web.WebAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription( "Classes and utilities to assist in creating Web API projects and libraries." )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany( "Jason Iverson Consulting" )]
 [assembly: AssemblyProduct("JM0ney.Framework.Web.WebAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "1.0.1.0" )]
+[assembly: AssemblyFileVersion( "1.0.1.0" )]
